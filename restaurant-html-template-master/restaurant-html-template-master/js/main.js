05517ef616c19cd63c64c6bab2ac77977c5752f9
Sergi Asensio <sasensio@innovadag.com>
2016-01-04 13:57:39 +0100

    test

---
+++ restaurant-html-template-master/restaurant-html-template-master/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+
+ $("#owl-example").owlCarousel({
+    // Most important owl features
+    items : 4,
+    pagination : true,
+    paginationSpeed : 1000,
+    navigation : true,
+    navigationText : ["","<i class='fa fa-angle-right'></i>"],
+    slideSpeed : 800,
+ });
+
+	$("#navigation").sticky({
+		topSpacing : 75,
+	});
+
+	$('#nav').onePageNav({
+		currentClass: 'current',
+		changeHash: false,
+		scrollSpeed: 15000,
+		scrollThreshold: 0.5,
+		filter: '',
+		easing: 'easeInOutExpo'
+	});
+
+     $('#top-nav').onePageNav({
+         currentClass: 'active',
+         changeHash: true,
+         scrollSpeed: 1200
+    });
+//Initiat WOW JS
+    new WOW().init();
+
+});
+
+function sendMail() {
+    var link = "mailto:me@example.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=" + escape(document.getElementById('myText').value)
+    ;
+
+    window.location.href = link;
+}
