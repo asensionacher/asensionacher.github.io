3a6ba5493496b023a96175d1f07b2ee0c2bc816a
Sergi Asensio <sergi.asensio.nacher@gmail.com>
2025-10-16 15:56:14 +0200

    Add type declarations for image and video files

---
+++ src/env.d.ts
@@ -0,0 +1,33 @@
+/// <reference types="astro/client" />
+
+declare module '*.svg' {
+    const content: {
+        src: string;
+        width: number;
+        height: number;
+    };
+    export default content;
+}
+
+declare module '*.png' {
+    const content: {
+        src: string;
+        width: number;
+        height: number;
+    };
+    export default content;
+}
+
+declare module '*.jpg' {
+    const content: {
+        src: string;
+        width: number;
+        height: number;
+    };
+    export default content;
+}
+
+declare module '*.mp4' {
+    const content: string;
+    export default content;
+}
